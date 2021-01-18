3c5fea5d2d209e6d5b5dfa06f285bc179fb0842f
Hunter Redding <hunterredding@VideoMac-2.local>
2021-01-18 15:20:39 -0500

    scrolling banner fix

--- src/components/ScrollingBanner.js
+++ src/components/ScrollingBanner.js
@@ -6,30 +6,30 @@ export default class ScrollingBanner extends Component {
         return (
             <div style={{width: "100%", height: "115px", backgroundColor: "#03305f", overflowX: "hidden"}}>
                 <div className={this.props.bClass}>
-                    <img alt="scrolling img 1" src="https://www.steveontv.com/images/SOT-1.jpg" />
-                    <img alt="scrolling img 2" src="https://www.steveontv.com/images/SOT-2.jpg" />
-                    <img alt="scrolling img 3" src="https://www.steveontv.com/images/SOT-3.jpg" />
-                    <img alt="scrolling img 4" src="https://www.steveontv.com/images/SOT-4.jpg" />
-                    <img alt="scrolling img 5" src="https://www.steveontv.com/images/SOT-5.jpg" />
-                    <img alt="scrolling img 6" src="https://www.steveontv.com/images/SOT-6.jpg" />
-                    <img alt="scrolling img 7" src="https://www.steveontv.com/images/SOT-7.jpg" />
-                    <img alt="scrolling img 8" src="https://www.steveontv.com/images/SOT-8.jpg" />
-                    <img alt="scrolling img 9" src="https://www.steveontv.com/images/SOT-9.jpg" />
-                    <img alt="scrolling img 10" src="https://www.steveontv.com/images/SOT-10.jpg" />
-                    <img alt="scrolling img 11" src="https://www.steveontv.com/images/SOT-11.jpg" />
-                    <img alt="scrolling img 12" src="https://www.steveontv.com/images/SOT-12.jpg" />
-                    <img alt="scrolling img 13" src="https://www.steveontv.com/images/SOT-13.jpg" />
-                    <img alt="scrolling img 14" src="https://www.steveontv.com/images/SOT-14.jpg" />
-                    <img alt="scrolling img 15" src="https://www.steveontv.com/images/SOT-15.jpg" />
-                    <img alt="scrolling img 16" src="https://www.steveontv.com/images/SOT-16.jpg" />
-                    <img alt="scrolling img 17" src="https://www.steveontv.com/images/SOT-17.jpg" />
-                    <img alt="scrolling img 18" src="https://www.steveontv.com/images/SOT-18.jpg" />
-                    <img alt="scrolling img 19" src="https://www.steveontv.com/images/SOT-19.jpg" />
-                    <img alt="scrolling img 1" src="https://www.steveontv.com/images/SOT-1.jpg" />
-                    <img alt="scrolling img 2" src="https://www.steveontv.com/images/SOT-2.jpg" />
-                    <img alt="scrolling img 3" src="https://www.steveontv.com/images/SOT-3.jpg" />
-                    <img alt="scrolling img 4" src="https://www.steveontv.com/images/SOT-4.jpg" />
-                    <img alt="scrolling img 5" src="https://www.steveontv.com/images/SOT-5.jpg" />
+                    <img alt="scrolling img 1" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/5Xo6d1bySiKKqytbC9L8_SOT-1.jpg" />
+                    <img alt="scrolling img 2" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/l8jibz3zRNqIE5q1t0UI_SOT-2.jpg" />
+                    <img alt="scrolling img 3" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/0fOWoofJTQmE4eFjl1jz_SOT-3.jpg" />
+                    <img alt="scrolling img 4" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/bvdJq0Y6QtyMSPGzOhp1_SOT-4.jpg" />
+                    <img alt="scrolling img 5" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/gHBk17cQemvsuRayThyE_SOT-5.jpg" />
+                    <img alt="scrolling img 6" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/R5ICzZ8MTCCpwDl4D5NA_SOT-6.jpg" />
+                    <img alt="scrolling img 7" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/j7HNiolFSCat8PAJbXdw_SOT-7.jpg" />
+                    <img alt="scrolling img 8" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/oPTKokSNCuYgSEfMHJKQ_SOT-8.jpg" />
+                    <img alt="scrolling img 9" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/q8N6UOwHRl60iR0eI6YO_SOT-9.jpg" />
+                    <img alt="scrolling img 10" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/r6rID3KpSdiK4PrFXs5N_SOT-10.jpg" />
+                    <img alt="scrolling img 11" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/OQUdYXKBTMi9M27cU5wb_SOT-11.jpg" />
+                    <img alt="scrolling img 12" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/GR0krXVTRJSFSp7zNkUD_SOT-12.jpg" />
+                    <img alt="scrolling img 13" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/lTUSUYf4RYegi6awukF7_SOT-13.jpg" />
+                    <img alt="scrolling img 14" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/Uj75IQN5Tn6UKMtOAfdg_SOT-14.jpg" />
+                    <img alt="scrolling img 15" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/UZISsfFSkyIOTCXcuIAV_SOT-15.jpg" />
+                    <img alt="scrolling img 16" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/Qd8yvEYuSoKDccGG4b04_SOT-16.jpg" />
+                    <img alt="scrolling img 17" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/zvDbuY2QZeX6Teuojpht_SOT-17.jpg" />
+                    <img alt="scrolling img 18" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/UTuC94bQ16tQ8ibHUEzA_SOT-18.jpg" />
+                    <img alt="scrolling img 19" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/mqdIOZ5qTwurkZhMEUEY_SOT-19.jpg" />
+                    <img alt="scrolling img 1" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/5Xo6d1bySiKKqytbC9L8_SOT-1.jpg" />
+                    <img alt="scrolling img 2" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/l8jibz3zRNqIE5q1t0UI_SOT-2.jpg" />
+                    <img alt="scrolling img 3" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/0fOWoofJTQmE4eFjl1jz_SOT-3.jpg" />
+                    <img alt="scrolling img 4" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/bvdJq0Y6QtyMSPGzOhp1_SOT-4.jpg" />
+                    <img alt="scrolling img 5" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/gHBk17cQemvsuRayThyE_SOT-5.jpg" />
                 </div>
             </div>
         )
